c97261a4b4a4f023fc3858b2114c3e0e90809170
Prathmesh <111947619+prthmh@users.noreply.github.com>
2024-03-17 19:41:21 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     appState: { items, category, search },
     setAppState,
   } = useData();
-  
+
   useEffect(() => {
     async function fetchData() {
       const res = await fetch("https://fakestoreapi.com/products");
@@ -21,15 +21,30 @@ function App() {
   }, []);
 
   const prodList = getItems(items, category, search);
-  
 
   return (
     <>
       <div className="container">
-        <header>
+        <div className="header">
+          <div className="top_banner">
+            <div className="top_banner_text">
+              <div>Best Sellers</div>
+              <div>Gift Ideas</div>
+              <div>New Releases</div>
+              <div>Todays Deals</div>
+              <div>Customer Service</div>
+            </div>
+          </div>
           <h1>Eflyer</h1>
           <NavBar />
-        </header>
+          <div className="banner">
+            GET STARTED
+            <br />
+            WITH YOUR FAVOURITE SHOPING
+          </div>
+          <button className="buy_btn">BUY NOW</button>
+        </div>
+        <h1>Man & Women Fashion</h1>
         <div className="products">
           {prodList?.map((prod) => (
             <div key={prod.id}>
